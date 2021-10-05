2ecfa558dd1900021c6354a20107f494385b4d2e
josh <jsooha00@gmail.com>
2021-10-06 05:16:41 +0900

    second_page_icon 디자인 변경

--- lib/page/second_page.dart
+++ lib/page/second_page.dart
@@ -4,26 +4,38 @@ class SecondPage extends StatelessWidget {
   final String title;
   final String body;
   final void Function() onTap;
+
   SecondPage({this.title, this.body, this.onTap});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(8.0),
+      padding: const EdgeInsets.all(6.0),
       child: InkWell(
         onTap: onTap,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              title,
-              style: const TextStyle(
-                fontWeight: FontWeight.bold,
-              ),
+        child: Padding(
+          padding: const EdgeInsets.only(left: 50, right: 50),
+          child: Card(
+            margin: const EdgeInsets.only(bottom: 10),
+            shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+            elevation: 5,
+            child: Container(
+              height: 50,
+              child: Align(
+                alignment: Alignment.center,
+                child: Column(
+                  children: [
+                    SizedBox(height: 5,),
+                    Text(
+                      title, style: const TextStyle(fontWeight: FontWeight.bold,
+                    fontSize: 22),),
+                    Text(body),
+                  ],
+                ),
+                ),
             ),
-            const SizedBox(height: 4),
-            Text(body),
-          ],
+          ),
         ),
       ),
     );
--- lib/page/second_page_icon.dart
+++ lib/page/second_page_icon.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:project/model/comment.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -5,6 +6,7 @@ import 'package:intl/intl.dart';
 
 class SecondPageIcon extends StatelessWidget {
   final Comment data;
+
   SecondPageIcon({this.data});
 
   @override
@@ -15,25 +17,32 @@ class SecondPageIcon extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(data.location),
-            const SizedBox(height: 16),
-            Text(_getDateRange()),
-            if (data.link != null)
-              TextButton(
-                onPressed:
-                _launchUrl,
-                style: TextButton.styleFrom(
-                  padding: EdgeInsets.zero,
-                ),
-                child: const Text(
-                  'Go to official website',
-                  style: TextStyle(color: Colors.blue),
-                ),
-              ),
-          ],
+        child: Card(
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(data.location,
+                style: TextStyle(
+                  fontSize: 45
+                ),),
+                const SizedBox(height: 16),
+                Text(_getDateRange()),
+                  TextButton(
+                    onPressed: _launchUrl,
+                    style: TextButton.styleFrom(
+                      padding: EdgeInsets.zero,
+                    ),
+                    child: const Text(
+                      'Go to official website',
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+              ],
+            ),
+          ),
         ),
       ),
     );
