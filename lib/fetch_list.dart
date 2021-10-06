8b5d9fede0e2434bf4318d2de04f70c5fbf47f13
josh <jsooha00@gmail.com>
2021-10-06 13:14:34 +0900

    fetch_list 필드 수정

--- lib/fetch_list.dart
+++ lib/fetch_list.dart
@@ -2,18 +2,13 @@ import 'dart:convert' as convert;
 import 'package:http/http.dart' as http;
 import 'package:project/model/comment.dart';
 
-  final String Url =
-    'https://raw.githubusercontent.com/junsuk5/mock_json/main/conferences.json';
-
 Future<List<Comment>> fetchList() async {
-  Uri url = Uri.parse(Url);
-  final response = await http.get(url);
+  final response = await http.get(
+      'https://raw.githubusercontent.com/junsuk5/mock_json/main/conferences.json');
 
   if (response.statusCode == 200) {
     Iterable jsonResponse = convert.jsonDecode(response.body);
-    return jsonResponse
-        .map((comment) => Comment.fromJson(comment))
-        .toList();
+    return jsonResponse.map((comment) => Comment.fromJson(comment)).toList();
   }
 
   throw Exception('Error');
